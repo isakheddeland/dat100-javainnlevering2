cd13bd8f8e79c15c4dd055a69c0ad8745351df8e
isakheddeland <isak.heddeland@gmail.com>
2025-09-27 18:09:22 +0200

    Fiksa feil me koden og maven

--- src/test/java/no/hvl/dat100/matriser/Hakon_test.java
+++
@@ -1,9 +0,0 @@
-package test.java.no.hvl.dat100.matriser;
-
-public class Hakon_test {
-    public static void main(String[] args) {
-
-        System.out.println("God aften Boys! ");
-        System.out.println("Hello world! ");
-    }
-}
